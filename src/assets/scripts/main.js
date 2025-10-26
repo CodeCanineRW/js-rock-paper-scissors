4a9b068c414b90ddaf31f1e3348d6a46341637fb
Ryan Winker <64046649+CodeCanineRW@users.noreply.github.com>
2025-10-26 14:27:29 -0700

    Create index and link main script with outline
    Created initial index.html and main.js files. Commented main.js with
    initial outline of code needed for the Rock Paper Scissors gameplay.

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,41 @@
+// -- Computer Choice Logic - getComputerChoice
+
+// Get random number between 0 and 3.
+// Rounded down to interger
+// Assign 0-1 as Rock, 1-2 as Paper, 2-3 as Scissors
+// Log Computer's choice
+
+
+
+// -- Human Choice Logic - getHumanChoice
+
+// Prompt user for a selection
+// Declare variable for player's choice
+// confirm string entered is a valid choice
+// Prompt again if invalid
+// Log Player's choice
+
+
+
+// -- Score Keeping Logic
+
+// Declare variable for Computer's score - computerScore
+// Declare variable for Human's score - humanScore
+// Start game with both values at 0
+
+
+
+// -- Gameplay Logic, Single Round - playRound(humanChoice, computerChoice)
+
+// Allow human and computer choices to be case insensitive
+// Compare selections to determine game result
+// Log round result with win or lose message
+// Increment score per round results
+
+
+
+// -- Gameplay Logic, Full Game
+
+// Repeat game round for 5 rounds
+// Track scores and compare after 5 rounds
+// Log and or alert game winner
