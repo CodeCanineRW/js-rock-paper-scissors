8a77f9992b6e398095deba5ce3f80cadae004f75
Ryan Winker <64046649+CodeCanineRW@users.noreply.github.com>
2025-12-05 18:11:45 -0800

    chore: remove excess comments and cleanup

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,11 +1,8 @@
 // -- Computer Choice Logic - getComputerChoice
 
-// Get random number between 0 and 3 Rounded down to integer
 function getRandomInt() {
     return Math.floor(Math.random() * 3);
 }
-
-// Assign 0-1 as Rock, 1-2 as Paper, 2-3 as Scissors and log computer's choice
 function getComputerChoice() {
     let computerChoice = getRandomInt();
     
@@ -14,21 +11,18 @@ function getComputerChoice() {
             computerChoice = "rock";
             console.log('Computer Selects: Rock');
             return "rock";
-            break;
 
         case 1:
             computerChoice = "paper";
             console.log('Computer Selects: Paper');
             return "paper";
-            break;
 
         case 2:
             computerChoice = "scissors";
             console.log('Computer Selects: Scissors');
             return "scissors";
-            break;
 
-        default: // Added to alert to and help debug unexpected result of computer selection
+        default:
             console.log('Computer was unable to make a selection');
             break;
     }
@@ -40,38 +34,32 @@ function getComputerChoice() {
 
 // -- Human Choice Logic - getHumanChoice
 
-// Prompt user for a selection
 function getPlayerSelection() {
     return prompt('Make your selection; Rock, Paper, or Scissors?');
 }
-// confirm string entered is a valid choice and log playerSelection
 function getHumanChoice() {
-    let playerSelection = getPlayerSelection().toLowerCase().trim(); // Allow human choice to be case insensitive, remove spaces
+    let playerSelection = getPlayerSelection().toLowerCase().trim();
 
     switch (playerSelection) {
         case 'rock':
             playerSelection = 'rock';
             console.log('Player Selects: Rock');
             return 'rock';
-            break;
 
         case 'paper':
             playerSelection = 'paper';
             console.log('Player Selects: Paper');
             return 'paper';
-            break;
 
         case 'scissors':
             playerSelection = 'scissors'
             console.log('Player Selects: Scissors');
             return 'scissors';
-            break;
 
         default:    // Prompt again if invalid
             alert(`"${playerSelection}" is not a valid choice. Please try again picking Rock, Paper, or Scissors.`);
             console.log(`User entered: "${playerSelection}" which is not a valid choice.`);
             return getHumanChoice(); // ??? New Choice was not evaluating and adding to score, added return ???
-            break;
     }
 }
 // getHumanChoice(); // **DELETE** calling function while developing to test human selection
@@ -80,10 +68,7 @@ function getHumanChoice() {
 
 // -- Score Keeping Logic
 
-// Start game with both values at 0
-// Declare variable for Computer's score - computerScore
 let computerScore = 0;
-// Declare variable for Human's score - humanScore
 let humanScore = 0;
 
 
@@ -114,16 +99,16 @@ function playRound(humanChoice, computerChoice) {
 // Increment score per round results
 
 function roundTies() {
-    console.log('The round ties');
+    console.log('   The round ties');
 }
 
 function roundComputer() {
-    console.log('The Computer won the round');
+    console.log('   The Computer won the round');
     ++computerScore;
 }
 
 function roundHuman() {
-    console.log('The player won the round')
+    console.log('   The player won the round')
     ++humanScore;
 }
 
@@ -133,9 +118,6 @@ function roundHuman() {
 
 // -- Gameplay Logic, Full Game - playGame()
 
-// Repeat game round for 5 rounds
-// Track scores and compare after 5 rounds
-// Log and or alert game winner
 
 let numRounds = 1;
 
